2af41a86bea0ac5bbe8accc818858268fe3bd119
Juan <jtrj33@gmail.com>
2017-11-07 14:27:55 -0600

    Update main function to run interpreter continuously

--- Interpreter/RunTestInterpreter.cpp
+++ Interpreter/RunTestInterpreter.cpp
@@ -10,19 +10,21 @@
 #include <sstream>
 #include "Interpreter.hpp"
 #include "Scanner.hpp"
+#include "Parser.hpp"
 #include "gtest/gtest.h"
 
 int main(int argc, char * argv[]) {
-    testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+//    testing::InitGoogleTest(&argc, argv);
+//    return RUN_ALL_TESTS();
 
-//    std::string txt;
-//    while(true) {
-//        std::cout << "Enter some mathematical expression: ";
-//        std::getline(std::cin, txt);
-//        Scanner* s = new Scanner(txt);
-//        Interpreter in = Interpreter(s);
-//        std::cout << "The result is: " << in.expr() << std::endl;
-//    }
+    std::string txt;
+    while(true) {
+        std::cout << ">> ";
+        std::getline(std::cin, txt);
+        Scanner* s = new Scanner(txt);
+        Parser*  p = new Parser(s);
+        Interpreter in = Interpreter(p);
+        std::cout << ">> " << in.interpret() << std::endl;
+    }
 }
 
